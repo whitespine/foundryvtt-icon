7fb93147e4afb2f15067308f613486bb78ecd4eb
WHITESPINE <jakehenry.tn@gmail.com>
2023-07-15 20:51:22 +0000

    DocClock now renamable

--- src/view/components/DocClock.svelte
+++ src/view/components/DocClock.svelte
@@ -2,11 +2,16 @@
     import { getContext } from "svelte";
     import Clock from "./Clock.svelte";
     import { resolveDotpath } from "../../util/paths";
+    import { updateDoc } from "../actions/update";
 
-    /** @type {string} */
+    /** @type {string} Path to the clock*/
     export let path;
 
+    /** @type {boolean} Should the name be an input? */
+    export let title_editable = true;
+
     let actor = getContext("tjs_actor");
+
 
     let clock;
     $: clock = resolveDotpath($actor, path);
@@ -17,7 +22,22 @@
     }
 </script>
 
-<Clock value={clock.value} size={clock.size} on:change={handleChange} >
-</Clock>
+<div>
+    {#if title_editable}
+        <input type="text" use:updateDoc={{doc: actor, path: `${path}.name`}} />
+    {:else}
+        <span>{clock.name}</span>
+    {/if}
+    <Clock value={clock.value} size={clock.size} on:change={handleChange} >
+    </Clock>
+</div>
+
+<style lang="scss">
+    div {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
 
 
