d81fe194d236c9221d8281fae49ab67f773f0114
WHITESPINE <jakehenry.tn@gmail.com>
2023-07-15 16:38:41 +0000

    Player sheet WIP copy

---
+++ src/view/sheets/ActorSheetApp.js
@@ -0,0 +1,48 @@
+import { SvelteApplication } from "#runtime/svelte/application";
+import { TJSDocument } from "#runtime/svelte/store/fvtt/document";
+import ActorSheetAppShell from "./ActorSheetAppShell.svelte";
+
+export default class TJSActorSheet extends SvelteApplication {
+  /**
+   * 
+   * @param {IconActor} actor Actor to display
+   * @param {object} options Application options
+   */
+  constructor(actor, options = {}) {
+    super({
+      svelte: {
+        props: {
+          actor_store: new TJSDocument(actor),
+        },
+      },
+    });
+  }
+
+  /**
+   * Default Application options
+   */
+  static ticker = 0;
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      // TODO: why don't we get the normal Foundry buttons in the window header?
+      // TODO: need a unique id per actor, otherwise Svelte won't let us open more than one at once
+      id: `actor-sheet-${this.ticker++}`,
+      classes: ["icon", "sheet", "actor"],
+      resizable: false,
+      minimizable: true,
+      popOut: false,
+      width: 800,
+      height: "auto",
+      positionOrtho: false,
+      transformOrigin: null,
+      title: "NEW ACTOR SHEET",
+      zIndex: 95,
+      svelte: {
+        class: ActorSheetAppShell,
+        target: document.body,
+        intro: true,
+        props: {},
+      },
+    });
+  }
+}
---
+++ src/view/sheets/ActorSheetAppShell.svelte
@@ -0,0 +1,25 @@
+<svelte:options accessors={true} />
+
+<script>
+    import { setContext } from "svelte";
+    import { TJSApplicationShell } from "#runtime/svelte/component/core";
+    // import DocStringField from "../../components/DocStringField.svelte";
+    import { scale } from "svelte/transition";
+    import PlayerSheet from "./PlayerSheet.svelte";
+
+    export let elementRoot = void 0;
+
+    /** @type {Readable<IconActor>} */
+    export let actor_store;
+
+    // For anything deeper than root doc
+    setContext("actor_store", actor_store);
+</script>
+
+<TJSApplicationShell bind:elementRoot transition={scale} transitionOptions={{ duration: 1000 }}>
+    <PlayerSheet actor={$actor_store} />
+</TJSApplicationShell>
+
+<style lang="scss">
+ 
+</style>
