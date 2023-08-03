ae7b7508f4bda2df2e8934b4e52cabe04b7ebcd4
WHITESPINE <jakehenry.tn@gmail.com>
2023-08-03 22:10:31 +0000

    Summons can now be clicked to open / dragged to map

--- src/view/components/combat/SummonDetail.svelte
+++ src/view/components/combat/SummonDetail.svelte
@@ -3,14 +3,29 @@
 
     // A summon. Though this might be an odd choice, explicitly NOT a tjs document!
     export let summon;
+
+    function openSheet() {
+        summon.sheet.render(true, {focus: true});
+    }
+
+    function setDragData(evt) {
+        evt.stopImmediatePropagation();
+        evt.dataTransfer.setData("text/plain", JSON.stringify(summon.toDragData()));
+    }
 </script>
 
-<span class="title">
+<span draggable=true on:click={openSheet} on:dragstart={setDragData}>
     {summon.name}
 </span>
 
 <style lang="scss">
-    .title {
+    span {
         font-weight: bold;
+        border-radius: 5px;
+        border: var(--primary-border);
+        background-color: var(--tertiary-background);
+        color: var(--tertiary-color);
+        padding: 2px;
+        cursor: pointer;
     }
 </style>
