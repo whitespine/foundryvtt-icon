10219250f40f350d3aad781a78225fe38e24c6d1
WHITESPINE <jakehenry.tn@gmail.com>
2023-08-18 21:17:15 +0000

    Relic model specced out

--- src/models/items/relic.js
+++ src/models/items/relic.js
@@ -1,4 +1,4 @@
-import { CastingStringField, FakeBoundedNumberField } from "../base";
+import { CastingStringField, ControlledLengthArrayField, FakeBoundedNumberField } from "../base";
 import { ItemModel } from "./item";
 
 
@@ -7,9 +7,20 @@ export class RelicModel extends ItemModel {
         return {
             ...super.defineSchema(),
             description: new foundry.data.fields.HTMLField(),
-
-            rank: new FakeBoundedNumberField({min: 1, max: 4}),
+            ranks: new ControlledLengthArrayField(new foundry.data.fields.HTMLField(), { length: 4 }),
+            rank: new FakeBoundedNumberField({ min: 1, max: 4 }),
             infused_dust: new FakeBoundedNumberField(),
         };
+    }
+
+    prepareDerivedData() {
+        // Set infusion max
+        if (this.rank === 3) {
+            this.infused_dust.max = 12;
+        } else if (this.rank === 4) {
+            this.infused_dust.max = 0;
+        } else {
+            this.infused_dust.max = 6;
+        }
     }
 }
