f408ad417b6816b74a2fe578ab696106dee3afbb
WHITESPINE <jakehenry.tn@gmail.com>
2023-08-18 20:53:18 +0000

    Better portrait

--- src/view/components/Portrait.svelte
+++ src/view/components/Portrait.svelte
@@ -2,18 +2,17 @@
     import { getContext } from "svelte";
 
     let doc = getContext("tjs_doc");
-    
+
     function onEdit(event) {
-        const attr = event.currentTarget.dataset.edit;
-        const current = foundry.utils.getProperty(this.object, attr);
         const fp = new FilePicker({
-            current,
+            current: $doc.img,
             type: "image",
-            callback: path => {
-            $doc.update({img: path})
+            displayMode: "tiles",
+            callback: (path) => {
+                $doc.update({ img: path });
             },
             top: this.y,
-            left: this.x + 100
+            left: this.x + 100,
         });
         return fp.browse();
     }
