f1c14d617d34713b398cf25251f2865f1e2284d8
WHITESPINE <jakehenry.tn@gmail.com>
2023-09-09 20:16:31 +0000

    more accurate

---
+++ src/util/stores/foundryui.js
@@ -0,0 +1,24 @@
+import { readable } from "svelte/store";
+
+export const SIDEBAR_LEFT = readable(0, update => {
+    const sidebar = $("#sidebar");
+
+    function doUpdate() {
+        update(sidebar.getBoundingClientRect().left || 0);
+    }
+
+    doUpdate();
+    Hooks.on("collapseSidebar", doUpdate);
+});
+
+export const HOTBAR_TOP = readable(0, update => {
+    const bottom = $("#hotbar");
+
+    function doUpdate() {
+        update(bottom.getBoundingClientRect().top || 0);
+    }
+
+    doUpdate();
+    // There's not actually a hook for this
+    // Hooks.on("collapseHotbar", doUpdate);
+});
