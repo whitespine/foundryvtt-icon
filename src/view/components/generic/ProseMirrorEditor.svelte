8ec41501345dde2daa5d8e05f1ebae89aee63668
WHITESPINE <jakehenry.tn@gmail.com>
2023-09-26 20:44:55 +0000

    Swapped to Tiny MCE editor

--- src/view/components/generic/ProseMirrorEditor.svelte
+++ src/view/components/generic/ProseMirrorEditor.svelte
@@ -3,7 +3,7 @@
 <script>
     import { ApplicationShell } from "#runtime/svelte/component/core";
 
-    import { TJSProseMirror } from "#standard/component";
+    import { TJSProseMirror, TJSTinyMCE } from "#standard/component";
 
     export let doc;
     export let path;
@@ -24,28 +24,41 @@
      */
     let options;
     $: options = {
-            document: doc, // An item to edit description; note: replace w/ valid doc.
-            fieldName: path, // Path to data in `a.b.c`; note: this is a v10 field name.
-            collaborate: true, // Enables collaboration; requires document.
-            button: !auto, // Show edit button to launch editor when hovered; when false editor is open by default.
-            // classes: ['foo', 'bar'],   // Adds additional classes to `.tjs-editor` element.
-            // clickToEdit: false,  // Clicking editor content initializes the editor; hides the edit button.
-            // DOMPurify,        // You can pass DOMPurify from `#runtime/dompurify though ProseMirror does
-            // essential client side sanitation; IE stripping `<script>` tags, etc.
-            editable: true, // Enable / disable editing
-            enrichContent: true, // The default is true, but if you set it to false content is not enriched.
-            initialSelection: "start", // The initial selection / cursor position: 'all', 'end', or 'start'.
-            // styles: { '--tjs-editor-toolbar-background': 'red' } // Apply any inline styles / CSS variables
-        };
-    </script>
+        document: doc, // An item to edit description; note: replace w/ valid doc.
+        fieldName: path, // Path to data in `a.b.c`; note: this is a v10 field name.
+        collaborate: false, // Enables collaboration; requires document.
+        button: !auto, // Show edit button to launch editor when hovered; when false editor is open by default.
+        // classes: ['foo', 'bar'],   // Adds additional classes to `.tjs-editor` element.
+        // clickToEdit: false,  // Clicking editor content initializes the editor; hides the edit button.
+        // DOMPurify,        // You can pass DOMPurify from `#runtime/dompurify though ProseMirror does
+        // essential client side sanitation; IE stripping `<script>` tags, etc.
+        editable: true, // Enable / disable editing
+        enrichContent: true, // The default is true, but if you set it to false content is not enriched.
+        initialSelection: "start", // The initial selection / cursor position: 'all', 'end', or 'start'.
+        // styles: { '--tjs-editor-toolbar-background': 'red' } // Apply any inline styles / CSS variables
+    };
+</script>
 
-<div>
-    <TJSProseMirror {options} />
+<div class="icon-editor">
+    <!--<TJSProseMirror {options} />-->
+    <TJSTinyMCE {options} />
 </div>
 
 <style lang="scss">
-    :global(.prosemirror .editor-container .editor-content) {
+    :global(.icon-editor .prosemirror .editor-container .editor-content) {
         position: relative;
+    }
+
+    :global(.icon-editor .editor) {
+        position: relative;
+        --tjs-editor-toolbar-button-color: var(--primary-color);
+        --tjs-editor-content-color: var(--primary-color);
+        --tjs-editor-toolbar-select-background: var(--secondary-background);
+        // --tjs-editor-active-overflow: unset;
+    }
+
+    :global(.icon-editor .editor-active) {
+        --tjs-editor-height: 200px;
     }
 
     div {
